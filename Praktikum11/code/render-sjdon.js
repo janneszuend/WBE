700cf6c07c05cb3708350af4b0b4ae058223a695
janneszuend <goalierebstei@gmail.com>
2023-12-02 20:07:21 +0100

    Update render-sjdon.js

--- Praktikum11/code/render-sjdon.js
+++ Praktikum11/code/render-sjdon.js
@@ -1,27 +1,27 @@
 function renderSJDON(element, appRoot) {
-  let [tag, props, ...children] = element
+  let [tag, props, ...children] = element;
 
-  let el = document.createElement(tag)
+  let el = document.createElement(tag);
   for (let p in props) {
-    el.setAttribute(p, props[p])
+    el.setAttribute(p, props[p]);
   }
 
   for (let child of children) {
-    let node = document.createElement(child[0])
-    let num = child.length
+    let node = document.createElement(child[0]);
+    let num = child.length;
     for (let i = 1; i < num; i++) {
-      if (typeof child[i] === 'string') {
-        node.textContent = child[i]
-      }else{
-        let attrs = child[i]
+      if (typeof child[i] === "string") {
+        node.textContent = child[i];
+      } else {
+        let attrs = child[i];
         for (let a in attrs) {
-          node.setAttribute(a, attrs[a])
+          node.setAttribute(a, attrs[a]);
         }
       }
     }
-    el.appendChild(node)
+    el.appendChild(node);
   }
-  appRoot.appendChild(el)
+  appRoot.appendChild(el);
 }
 
-export {renderSJDON}
+export { renderSJDON };
